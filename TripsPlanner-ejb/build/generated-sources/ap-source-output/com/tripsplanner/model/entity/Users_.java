1210d1da259622012b362a0c962296f9fb59a3ee
sp159589 <sartor.gabriele@educ.di.unito.it>
2018-05-08 09:43:49 +0200

    Added template files.

--- TripsPlanner-ejb/build/generated-sources/ap-source-output/com/tripsplanner/model/entity/Users_.java
+++ TripsPlanner-ejb/build/generated-sources/ap-source-output/com/tripsplanner/model/entity/Users_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-07T17:52:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-08T09:37:50")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
