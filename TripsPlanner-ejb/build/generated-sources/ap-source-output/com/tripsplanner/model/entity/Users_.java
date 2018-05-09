62cd19967381cac76e3ebae7e4b06913a7ee15b5
Riccardo Renzulli <renzulli.riccardo@gmail.com>
2018-05-09 15:20:34 +0200

    Modified login page

--- TripsPlanner-ejb/build/generated-sources/ap-source-output/com/tripsplanner/model/entity/Users_.java
+++ TripsPlanner-ejb/build/generated-sources/ap-source-output/com/tripsplanner/model/entity/Users_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T09:42:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T15:04:11")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
