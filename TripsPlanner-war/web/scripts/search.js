6bc23ec17e2517ed09fab281358a3be43cdd8252
the-silent-fox <sartor.gabriele23@gmail.com>
2018-05-11 17:26:05 +0200

    SearchServlet gets the departure and destination city.

---
+++ TripsPlanner-war/web/scripts/search.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function buttonSearch() {
+    console.log(document.getElementById('departure_city').value);
+    console.log(document.getElementById('destination_city').value);
+    
+  }
