b63850f86ee50d04d2dcf3a9b86ead49830f76a5
Riccardo Renzulli <renzulli.riccardo@gmail.com>
2018-05-23 17:26:27 +0200

    create user jsp

--- TripsPlanner-ejb/src/main/java/com/tripsplanner/model/bean/MemoryBean.java
+++ TripsPlanner-ejb/src/main/java/com/tripsplanner/model/bean/MemoryBean.java
@@ -30,9 +30,17 @@ import javax.servlet.http.Part;
 
 
 /**
- *
- * @author riccardo
+ * Authors: Giovanni Bonetta, Riccardo Renzulli, Gabriele Sartor<br>
+ * Università degli Studi di Torino<br>
+ * Department of Computer Science<br>
+ * Sviluppo Software per Componenti e Servizi Web<br>
+ * Date: May 2018<br><br>
+ * <p/>
+ * giovanni.bonetta@edu.unito.it<br>
+ * riccardo.renzulli@edu.unito.it<br>
+ * gabriele.sartor@edu.unito.it<br><br>
  */
+
 @Stateless
 public class MemoryBean implements MemoryBeanLocal {
     
--- TripsPlanner-war/src/main/java/com/tripsplanner/servlet/MemoryServlet.java
+++ TripsPlanner-war/src/main/java/com/tripsplanner/servlet/MemoryServlet.java
@@ -26,9 +26,17 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 /**
- *
- * @author riccardo
+ * Authors: Giovanni Bonetta, Riccardo Renzulli, Gabriele Sartor<br>
+ * Università degli Studi di Torino<br>
+ * Department of Computer Science<br>
+ * Sviluppo Software per Componenti e Servizi Web<br>
+ * Date: May 2018<br><br>
+ * <p/>
+ * giovanni.bonetta@edu.unito.it<br>
+ * riccardo.renzulli@edu.unito.it<br>
+ * gabriele.sartor@edu.unito.it<br><br>
  */
+
 @MultipartConfig
 public class MemoryServlet extends HttpServlet {
 
