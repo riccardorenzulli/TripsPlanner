aae26d241510aa1b33236e13191f2d3fd14d16b3
giobin <bonettagiovanni@gmail.com>
2018-06-01 17:07:36 +0200

    loading page after hotellist

---
+++ TripsPlanner-war/src/main/webapp/assets/js/loadingPage.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function loadingPage(){
+
+    document.getElementById("main_cont").style.display = "none";
+    document.getElementById("load_cont").style.display = "block";
+
+}
